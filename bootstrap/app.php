a13b60d36e695ce96d69eb25a12426093882a59b
max <max@example.com>
2026-05-28 22:50:52 +0200

    mod2.5

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Dice a Laravel di fidarsi dei proxy di Render per l'HTTPS
+    $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
