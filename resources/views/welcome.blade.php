d097bc9d035e5e7eff366e2f0b30c95615d162d5
max <max@example.com>
2026-06-08 17:55:52 +0200

    fix: aggiungere link all'icona del logo nella sezione head del layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="/img/logo-regione.png">
     <title>Protezione Civile - Coordinamento Vesuvius</title>
     
     <!-- Google Fonts: Inter per un look moderno e pulito -->
