e702545955ccddfe6bc01bc0bdd05a38b10b4e5b
Max <max@example.com>
2026-06-12 18:51:58 +0200

    feat: implementare la gestione del refresh della navigazione dell'app e salvare la vista in attesa

--- resources/js/app.js
+++ resources/js/app.js
@@ -665,6 +665,7 @@ async function checkDatabaseConnection(forLoginScreen = false) {
 }
 
 async function bootstrapApp(user) {
+    appNavigationRefreshEnabled = false;
     await loadUserProfile(user);
 
     if (!currentUserProfile) {
@@ -683,6 +684,12 @@ async function bootstrapApp(user) {
     applyRoleBasedUI();
     await checkDatabaseConnection();
     await fetchDataFromSupabase();
+    const pendingViewAfterRefresh = sessionStorage.getItem(PENDING_VIEW_AFTER_REFRESH_KEY);
+    if (pendingViewAfterRefresh) {
+        sessionStorage.removeItem(PENDING_VIEW_AFTER_REFRESH_KEY);
+        switchTab(pendingViewAfterRefresh);
+    }
+    appNavigationRefreshEnabled = true;
     startRealtimeClock();
     startSquadreAibScadenzaTimer();
     return true;
@@ -851,6 +858,8 @@ let pendingPdfDelivery = null;
 let pendingPdfServizioId = null;
 let pendingVolontarioDocumentiId = null;
 let squadreAibScadenzaTimer = null;
+const PENDING_VIEW_AFTER_REFRESH_KEY = 'pc_pending_view_after_refresh';
+let appNavigationRefreshEnabled = false;
 
 const ICON_EDIT = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" /></svg>`;
 const ICON_EYE = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" /><path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" /></svg>`;
@@ -2387,6 +2396,14 @@ function switchTab(tabId) {
         else if (canAccessMagazzino()) tabId = 'magazzino';
         else if (canAccessVolontari()) tabId = 'volontari';
         else tabId = 'mezzi';
+    }
+
+    const currentTab = document.querySelector(".tab-content:not(.hidden)");
+    const currentTabId = currentTab?.id?.replace(/^tab-/, "");
+    if (appNavigationRefreshEnabled && currentTabId && currentTabId !== tabId) {
+        sessionStorage.setItem(PENDING_VIEW_AFTER_REFRESH_KEY, tabId);
+        window.location.reload();
+        return;
     }
 
     // Nascondi tutti i contenuti delle tab
