d204b359ecf32f86d519f03f0dfddbe315be3154
Max <max@example.com>
2026-07-06 17:25:00 +0200

    Refactor code structure for improved readability and maintainability

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -14,11 +14,11 @@
                    <!-- <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-amber-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.57-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
                     </svg>-->
-                    <img src="/img/logo-regione.png" alt="Logo Regione Campania" class="w-50 h-50 object-contain">
+                    <img src="/img/logoapp.png" alt="Logo Regione Campania" class="w-50 h-50 object-contain">
                 </div>
-                <h1 class="text-3xl font-extrabold text-white tracking-tight">{{ config('app.name') }}</h1>
+               <!-- <h1 class="text-3xl font-extrabold text-white tracking-tight">{{ config('app.name') }}</h1>
                 <h6 class="text-sm text-slate-600 mt-1">Vesuvius Integrated Governance & Operational Response</h6>
-                <p class="text-sm text-slate-400 mt-2 font-medium">Protezione Civile — Accesso Operatori</p>
+                <p class="text-sm text-slate-400 mt-2 font-medium">Protezione Civile — Accesso Operatori</p>-->
             </div>
 
             <!-- Card Login -->
