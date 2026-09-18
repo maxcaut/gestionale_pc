a1ead776d5090c067fd73c187a38e597e3450348
Max <max@example.com>
2026-09-18 21:49:48 +0200

    feat: add surname configuration to app settings

--- config/app.php
+++ config/app.php
@@ -14,6 +14,7 @@ return [
     */
 
     'name' => env('APP_NAME', 'VIGOR'),
+    'surname' => env('APP_SURNAME', 'Coordinamento Vesuvius'),
 
     /*
     |--------------------------------------------------------------------------
